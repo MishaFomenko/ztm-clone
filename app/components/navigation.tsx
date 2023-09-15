6c50199bf97d9bdb4a8436edba2aaa12084ba5b2
Mykhailo Fomenko <misha.fomenko00@gmail.com>
2023-09-14 23:24:38 -0400

    adding nav bar

---
+++ app/components/navigation.tsx
@@ -0,0 +1,20 @@
+
+
+export default function Navigation() {
+    return(
+        <div className='navigation-bar'>
+
+            <div className='logo'>ZTM</div>
+            <div className='navigation-buttons'>
+                <button>Academy</button>
+                <button>Testimonials</button>
+                <button>Blog</button>
+                <button>Cheat Sheets</button>
+                <button>Community</button>
+                <button className='signin'>SIGN IN</button>
+                <button className='join-ztm'>JOIN ZERO TO MASTERY</button>
+            </div>
+
+        </div>
+    )
+}
